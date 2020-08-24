09d0c806bdf9def92314928a63d18d2e35423be1
Tackii <nathanschostack@hotmail.com>
2020-08-24 17:56:50 -0300

    'interfacepronta'

--- Veiculo.java
+++ Veiculo.java
@@ -1,3 +1,10 @@
 public interface Veiculo {
-    
+    String getMarca();
+	String getModelo();
+	int getAno();
+	String getCor();
+	double getMotor();
+	int getPreco();
+	String getPlaca();
+	int getNumRodas();
 }
