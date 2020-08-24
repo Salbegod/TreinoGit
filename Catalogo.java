9431c546803c308895419b3b986b250dbd854d35
Lucas Pereira Salbego <lucas.salbego99@edu.pucrs.br>
2020-08-24 18:36:35 -0300

    Integracao da interface ao catalogo

--- Catalogo.java
+++ Catalogo.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Catalogo {
-    
+
+    private String modelo;
+    private String ano;
+    private String cor;
+    private double motor;
+    private int preco;
+    private String placa;
+    private int numRodas;
+    public static List<Veiculo> Veiculos = new ArrayList<>();
+
+    @Override
+    public String toString() {
+
+        return this.modelo + " " + this.ano + " " + this.cor + " " + this.motor + " " + this.preco + " " + this.placa + " " + this.numRodas;
+    }
 }
